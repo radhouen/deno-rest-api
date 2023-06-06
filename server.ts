090e04b5fc703aa1922414a8c64bc69d7ce13dff
horlatayorr <horlatayorr@gmail.com>
2023-06-06 10:20:26 +0100

    server.ts updated

--- server.ts
+++ server.ts
@@ -1,10 +1,30 @@
-import { Application, router } from "https://deno.land/x/oak/mod.ts";
+/**import { Application, router } from "https://deno.land/x/oak/mod.ts";
 //#region import router from './routes.ts';
 
 const app = new Application();
-const port = 8000 
+//#region const port = 8000 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 console.log(`Server running in port ${port}`);
-await app.listen({ port });
+app.listen({ port: 8000 });*/
+
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+router.get("/", (ctx) => {
+  ctx.response.body = `<!DOCTYPE html>
+    <html>
+      <head><title>Hello oak!</title><head>
+      <body>
+        <h1>Hello oak!</h1>
+      </body>
+    </html>
+  `;
+});
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 8080 });
