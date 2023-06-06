ba18bc77edb6e8d8230d60f704b41eab2a759d36
horlatayorr <horlatayorr@gmail.com>
2023-06-06 11:25:22 +0100

    server.ts

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ app.use(router.allowedMethods());
 console.log(`Server running in port ${port}`);
 app.listen({ port: 8000 });*/
 
-import { Application, Router } from "https://deno.land/x/oak@v12.4.0/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
 
 const router = new Router();
 router.get("/", (ctx) => {
