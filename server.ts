1e130fd12a72a9d7700e63c2643f30ce40b7408a
horlatayorr <horlatayorr@gmail.com>
2023-06-06 09:58:38 +0100

    changes made to server.ts

--- server.ts
+++ server.ts
@@ -1,5 +1,4 @@
-/**import { Application } from "https://deno.land/std@0.188.0/dotenv/mod.ts";*/
-import * as mod from "https://deno.land/std@0.190.0/dotenv/mod.ts";
+import { Application } from "https://deno.land/std@0.188.0/dotenv/mod.ts";
 import router from './routes.ts';
 
 const app = new Application();
